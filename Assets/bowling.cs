90add25ac2a8cf726adfb11959ebc541308e0cdd
mbl525 <mbl525@york.ac.uk>
2025-10-22 10:54:44 +0100

    Update bowling.cs

--- Assets/bowling.cs
+++ Assets/bowling.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class bowling : MonoBehaviour
 {
+    float zSpeed = 200.0f;
+    public float xAxis = -0.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +11,29 @@ public class bowling : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // controls ball rotation
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Debug.Log("Hai");
+            xAxis -= 5
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            xAxis += 5
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            zSpeed -= 50;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            zSpeed =+ 50;
+        }
+
+        if (Input.GetKeyDown("space")) {
+            Rigidbody rb = GetComponent<Rigidbody> ();
+            rb.AddForce(new Vector3(xAxis, 0.0f, -zSpeed));
+        }
     }
 }
